bc4f5858be14587795e6a8c1c0ef9327b4f477d2
MojizGaditek <35929797+MojizGaditek@users.noreply.github.com>
2018-02-02 21:11:23 +0500

    Updated Loin.py
    Deleted some comments to start building jenkins build

--- Login.py
+++ Login.py
@@ -1,11 +1,6 @@
 # ScriptName : Login.py
 from selenium import webdriver
 
-# Following are optional required
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import Select
-# from selenium.common.exceptions import NoSuchElementException
-
 url = "https://talent.gaditek.com/"
 Email = "smojizmehdi@gmail.com"
 password = "mojizmehdi1"
